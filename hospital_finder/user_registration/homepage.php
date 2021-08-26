6607e1302a160c7592c30d203f84b4a88d079592
Jafrin Akter Alo <81787520+JafrinAlo@users.noreply.github.com>
2021-08-26 22:31:09 +0600

    Add files via upload

---
+++ hospital_finder/user_registration/homepage.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Homepage</title>
+        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
+        <link rel="stylesheet" href="hpstyle.css">
+    </head>
+    <body>
+        <section>
+            <div class="circle"></div>
+            <header>
+                <h2>Hospital Finder</h2>
+                <ul class="navigation">
+                    <li><a href="login.php">Sign Up/Login</a></li>
+                    <li><a href="#">Read blogs</a></li>
+                    <li><a href="#">What's new!</a></li>
+                    <li><a href="#">About us</a></li>
+                </ul>
+            </header>
+            <div class="search">
+                <div class="icon"></div>
+                <div class="input"></div>
+            </div>
+            <div class="content"></div>
+        </section>
+    </body>
+</html>
